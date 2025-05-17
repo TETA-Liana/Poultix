77e4d59fcfaff0249ab35d70ee417108d5d7098f
goal651 <bugiriwilson651@gmail.com>
2025-05-17 04:29:00 +0200

    refactor: projection organization changed

---
+++ components/news/News.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+
+export default function NewsComponent() {
+    return (
+        <View>
+            <View>New disease outbreak in Muhanga</View>
+            <View>
+                {/* Image sections */}
+            </View>
+        </View>
+    )
+}
